c22cde4878ffe9f709545b7095ae172a3b9ceb84
Sadeep Mihiranga <sandeep@hatchyard.io>
2023-03-08 23:06:42 +0530

    Initial commit

---
+++ kafka-consumer-database/src/main/java/lk/sadeep/KafkaConsumerDatabaseApplication.java
@@ -0,0 +1,12 @@
+package lk.sadeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaConsumerDatabaseApplication.class, args);
+    }
+}
---
+++ src/test/java/lk/sadeep/SpringBootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.sadeep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
